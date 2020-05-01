27cfec6237f7a0822202d5cf4709e96eb817c1ff
Sergio Robles <sroblesc@gmail.com>
2020-05-01 12:43:06 +0100

    Saved

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+class MP3Importer 
+
+    attr_accessor :file
+
+    def initialize(file)
+        @file = file
+
+    end
+
+    def files 
+
+    end
+
+    def import 
+
+    end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song 
+
+    attr_accessor :artist, :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        
+        
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def self.new_by_filename
+
+    end
+
+    def artist_name 
+
+    end
+
+end
+
+
+
